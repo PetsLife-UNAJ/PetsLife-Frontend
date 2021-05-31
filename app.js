bfac78dee3a4ac687f5ca9f074f26a2d4f2adda9
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2021-05-30 21:01:32 -0300

    add menu principal and forms

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ app.use( express.static(__dirname + '/src'))
 //Ejemplo
 app.use('/home', (req,res)=>{
   res.sendFile(path.join(__dirname, '/src/pages/home.html'));
-
-}) //
+}) 
+//
+app.use('/add-client', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/src/pages/clinic-history/add-client.html'));
+}) 
+app.use('/add-mascota', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/src/pages/clinic-history/add-mascota.html'));
+}) 
 
 app.use('/', (req,res)=>{
   res.sendFile(path.join(__dirname, '/src/index.html'));
