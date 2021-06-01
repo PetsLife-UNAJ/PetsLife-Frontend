8ee683176ab5e2462accb4e3fa40004d0b1bb0f7
julinuzzo19 <julinuzzo19@gmail.com>
2021-06-01 16:57:22 -0300

    replace index page with home page

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const path=require('path');
+const path = require("path");
 
-app.use( express.static(__dirname + '/src'))
+app.use(express.static(__dirname + "/src"));
 
 //Ejemplo
-app.use('/home', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/src/pages/home.html'));
-}) 
+app.use("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/pages/home.html"));
+});
 //
-app.use('/add-client', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/src/pages/clinic-history/add-client.html'));
-}) 
-app.use('/add-mascota', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/src/pages/clinic-history/add-mascota.html'));
-}) 
+app.use("/add-client", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/src/pages/clinic-history/add-client.html")
+  );
+});
+app.use("/add-mascota", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/src/pages/clinic-history/add-mascota.html")
+  );
+});
 
-app.use('/', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/src/index.html'));
-
-})
-
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/pages/home.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}/home`);
 });
