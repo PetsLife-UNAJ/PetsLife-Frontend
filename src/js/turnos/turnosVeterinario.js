1cda7f91b4b0a63aaaf1bcc784b4cea6923c3961
julinuzzo19 <julinuzzo19@gmail.com>
2021-06-24 02:35:15 -0300

    added all methods in attend mascota

--- src/js/turnos/turnosVeterinario.js
+++ src/js/turnos/turnosVeterinario.js
@@ -46,46 +46,108 @@ const crearModal = (turno) => {
   <div class="modal-dialog">
     <div class="modal-content modal-listado-content">
       <div class="modal-header">
-        <h5 class="modal-title">${turno.horaInicio}</h5>
+        <h5 class="modal-title">Turno de ${turno.mascotaNombre}</h5>
       </div>
       <div class="modal-body">
-        <div class="container">
-          <div class="container-fluid">
-            <div
-              class="
-                row
-                justify-content-center
-                row-cols-auto
-                fw-bold
-                mt-4
-                text-center
-                fs-6
-              "
-            >
-              <div class="col-2 border border-dark p-1">Horario</div>
-              <div class="col-2 border border-dark p-1">Mascota</div>
-              <div class="col-2 border border-dark p-1">Veterinario</div>
-              <div class="col-2 border border-dark p-1">Cliente</div>
-              <div class="col-2 border border-dark p-1">Telefono</div>
-              <div class="col-2 border border-dark p-1">Consultorio</div>
-            </div>
+        <div class="container-sm">
+          
+        <form>
+            <div class="mb-3">
+                <label for="registro-input" class="form-label fw-bold">Agregar un registro</label>
+                <textarea type="email" class="form-control" id="registro-input-${turno.turnoId}" rows="3" required></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="tratamiento-input" class="form-label fw-bold">Agregar un tratamiento</label>
+                <textarea class="form-control" id="tratamiento-input-${turno.turnoId}" required ></textarea>
+              </div>
 
-            <section id="rows-turnos"></section>
-          </div>
         </div>
 
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
-            >Cerrar</button
+        <div class="modal-footer justify-content-center">          
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="btn-turno-${turno.turnoId}"
+            >Terminar</button
           >
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal"
+          >Cancelar</button
+        >
+        </form>
         </div>
       </div>
     </div>
   </div>   
   `;
   listaModal.appendChild(element);
+  const btnTurno = document.getElementById(`btn-turno-${turno.turnoId}`);
+  btnTurno.onclick = () => {
+    const tratamiento = document.getElementById(
+      `tratamiento-input-${turno.turnoId}`
+    ).value;
+    const registro = document.getElementById(
+      `registro-input-${turno.turnoId}`
+    ).value;
+    const mascotaId = turno.mascotaId;
+    const turnoId = turno.turnoId;
+
+    let data = {tratamiento, registro, mascotaId, turnoId};
+    agregarRegistro(data);
+  };
 };
 
-window.onload = async () => {
+const agregarRegistro = async (data) => {
+  const historiaClinica = await fetch(
+    `https://localhost:44314/api/HistoriaClinica/${data.mascotaId}`
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => console.log(err));
+
+  let registro = {
+    analisis: data.registro,
+    historiaClinicaId: historiaClinica[0].historiaClinicaId
+  };
+
+  let registroJson = JSON.stringify(registro);
+
+  const registroResponse = await fetch(`https://localhost:44314/api/Registros`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json;charset=UTF-8'},
+    body: registroJson
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => console.log(err));
+
+  let tratamiento = {
+    registroId: registroResponse.registroId,
+    descripcion: data.tratamiento
+  };
+
+  let tratamientoJson = JSON.stringify(tratamiento);
+  fetch(`https://localhost:44314/api/Tratamiento`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json;charset=UTF-8'},
+    body: tratamientoJson
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.status == 201) {
+        console.log('Tratamiento registrado con exito');
+      }
+    })
+    .catch((err) => console.log(err));
+
+  fetch(`https://localhost:44314/api/Turno/${data.turnoId}`, {
+    method: 'DELETE'
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+window.onload = () => {
   getTurnosVeterinario(1001);
 };
