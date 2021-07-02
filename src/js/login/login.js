c15b497d46fab3b08ea0976b9dbb007d24e11915
julinuzzo19 <julinuzzo19@gmail.com>
2021-07-01 22:52:22 -0300

    added validation login

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -1,26 +1,19 @@
-import {URL_API_LOGIN} from '../constants.js';
+import {login} from '../services/loginService.js';
 const formLogin = document.getElementById('form-login');
 
-formLogin.onsubmit = (e) => {
+formLogin.onsubmit = async (e) => {
   e.preventDefault();
 
   let {correo, password} = formLogin.elements;
 
   let cuenta = {email: correo.value, password: password.value};
 
-  let cuentajson = JSON.stringify(cuenta);
-
-  fetch(URL_API_LOGIN, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    },
-    body: cuentajson
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      console.log(response);
-      localStorage.setItem('usuario', JSON.stringify(response));
-    })
-    .catch((err) => console.log(err));
+  let response = await login(cuenta);
+  if (response) {
+    console.log(response);
+    localStorage.setItem('usuario', JSON.stringify(response));
+    window.location.href = 'home';
+  } else {
+    document.getElementById('estado-login').innerHTML = 'No se ha podido registrar.';
+  }
 };
