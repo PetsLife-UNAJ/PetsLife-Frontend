2eee93d229cef5f0101b6f2e79711364aca454b5
Nicolas <acu.nicolas.1998@gmail.com>
2021-07-13 16:29:17 -0300

    form register

--- src/js/login/register.js
+++ src/js/login/register.js
@@ -2,9 +2,56 @@ import {registerUser} from '../services/registerService.js';
 
 const formRegister = document.getElementById('form-register');
 
+const checkForm = (form) => {
+  form.addEventListener("submit", (event) => {
+    console.log(form.checkValidity());
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      } 
+      else {
+          event.preventDefault();
+          enviarFormRegister(form);
+      }
+
+    form.classList.add("was-validated");
+    }, false
+  );
+};
+
+
+checkForm(formRegister);
+/*
 formRegister.onsubmit = async (e) => {
   e.preventDefault();
 
+  let nombre = formRegister.elements.nombre.value;
+  let apellido = formRegister.elements.apellido.value;
+  let dni = formRegister.elements.dni.value;
+  let email = formRegister.elements.email.value;
+  let telefono = formRegister.elements.telefono.value;
+  let password = formRegister.elements.contraseña.value;
+  let sexo = formRegister.elements.sexo.value;
+  let direccion = formRegister.elements.direccion.value;
+
+  let registro = {
+    nombre,
+    apellido,
+    dni,
+    email,
+    telefono,
+    password,
+    sexo,
+    direccion
+  };
+
+  registerUser(registro);
+};
+*/
+
+
+
+const enviarFormRegister = (formRegister) => {
   let nombre = formRegister.elements.nombre.value;
   let apellido = formRegister.elements.apellido.value;
   let dni = formRegister.elements.dni.value;
